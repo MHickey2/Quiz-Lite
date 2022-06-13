f4f0a49d022a0dc4b3a01201866a67695bf54679
Michelle Hickey <michellehickey2@yahoo.ie>
2022-06-13 20:09:03 +0000

    working on the choice events and linking modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ var questions = [];
 var userName;
 var questionNumber;
 var quiz;
-var correctAnswer;
 var index = 0;
+var correctAnswer;
+var selectedChoice;
 
 
 async function getQuestions() {
@@ -17,6 +18,7 @@ function getNextQuestion(index) {
     //getCurrent question
     const currentQuestion = questions.results[index]
     //put correct answers in list with incorrect answers
+    correctAnswer = currentQuestion.correct_answer;
     let choices = currentQuestion.incorrect_answers.concat(currentQuestion.correct_answer);
     choices.sort(() => Math.random() - 5);
 
@@ -57,7 +59,7 @@ async function init() {
     getNextQuestion(0);
     startGame();
     choiceListeners();
-    checkAnswers();
+    //checkAnswers();
 
 }
 
@@ -67,12 +69,14 @@ init()
 
 
 function checkAnswers() {
-    console.log("This is the correct answer", currentQuestion.correct_answer)
+    
 
     let button1 = document.getElementById("answer1");
-    button1.addEventListener("click", () => {
+    button1.addEventListener("click", event => {
+        const selectedChoice = e.target
+        
 
-        if (button1.value === currentQuestion.correct_answer) {
+        if (selectedChoice === correctAnswer.value) {
             alert("you are correct");
             button1.style.background = "green";
             // incrementScore();
@@ -85,6 +89,24 @@ function checkAnswers() {
     })
 
 }
+
+/*choices.forEach(choice => {
+    //for each choice add a click event listener, pass the event to the callback function
+    choice.addEventListener("click", event => {
+        if (!acceptingAnswers) return
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset('questions')
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+        if (classToApply === "correct"){
+            alert("your are right")
+        } else alert("you are wrong")
+    })
+}) */   
+        
+
+
 
 //Get username of the user and send to welcome message on game page
 
