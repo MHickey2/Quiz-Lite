865383ff5c480308f099977840205857224d0bbb
Michelle Hickey <michellehickey2@yahoo.ie>
2022-07-01 18:19:07 +0000

    Tidy up of javascript code to make it flow better and be more readable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,36 @@
-//modals to get user categories and difficulty levels
+const img = document.getElementById('daynight');
 const modal = document.querySelector('#modal');
 const openModal = document.querySelector('.open-button');
 const closeModal = document.querySelector('.close-button');
 const modal2 = document.querySelector('#modal2');
 const openModal2 = document.querySelector('.open-button2');
 const closeModal2 = document.querySelector('.close-button2');
+const categoryButton = document.getElementById('category-btn');
+const difficultyButton = document.getElementById('difficulty-btn');
+const targetDiv3 = document.getElementById('hiddenResults');
+const targetDiv4 = document.getElementById('verify-answer');
+const restartButton = document.getElementById('restart-btn');
 const maxQuestions = 10;
+
 let answers = [];
 let questions = [];
 let index = 0;
 let correctAnswer;
 let currentQuestion;
-var category;
-var difficulty;
 let questionNumber = 1;
 //let soundcorrect = new Audio("/audio/correct.mp3");
 //let soundwrong = new Audio("/audio/wrong.mp3");
 let finalScore;
 let timer;
 let timeLeft = 60;
+var category;
+var difficulty;
 
-/*function getUserName() {
-    let username = prompt("Enter a username");
-    if (username != '') {
-        // Save data to sessionStorage
-        sessionStorage.setItem('username', username);
-        document.getElementById('welcomeText').innerHTML = 'Welcome ' + username + ' to Quiz-Lite';
-    } else {
-        alert('Username cannot be blank!');
-    }
-}*/
+img.addEventListener('click', changeColourScheme);
+categoryButton.addEventListener('click', getCategory);
+difficultyButton.addEventListener('click', getDifficultyLevel);
+restartButton.addEventListener('click', replay);
+
 //Function to get user name and welcome them personally to the game
 function getUserName() {
     let username = '';
@@ -38,7 +39,6 @@ function getUserName() {
         username = prompt('Enter your username:').trim().toLowerCase();
         if (alreadyShownPrompt) {
             alert('username can not be blank!');
-
         }
         alreadyShownPrompt = true
     }
@@ -49,43 +49,17 @@ function getUserName() {
 
 getUserName();
 
-//Modal for categories
-openModal.addEventListener('click', () => {
-    modal.showModal();
-});
-
-closeModal.addEventListener('click', () => {
-    modal.close();
-});
-
-//Modal2 for difficulty
-openModal2.addEventListener('click', () => {
-    modal2.showModal();
-});
-
-closeModal2.addEventListener('click', () => {
-    modal2.close();
-});
-
-const img = document.getElementById('daynight');
-img.addEventListener('click', changeColourScheme)
-
 //function to choose dark-mode
 function changeColourScheme() {
     var element = document.body;
     element.classList.toggle('dark-mode');
+    // Save data to sessionStorage
+    localStorage.setItem('element', 'dark-mode');    
 }
 
 changeColourScheme();
 
-// Save data to sessionStorage
-localStorage.setItem('element', 'dark-mode');
-//let theme = localStorage.getItem('element');
-
-const categoryButton = document.getElementById('category-btn');
-categoryButton.addEventListener('click', getCategory);
-
-//function to get category
+//function to get category for the fetch API request
 function getCategory() {
     let categorylevel = document.forms[0];
     category = '';
@@ -100,15 +74,11 @@ function getCategory() {
     sessionStorage.setItem('category', category);
     // document.getElementById("resultsCategory").value = "You have chosen:" + category;
     document.getElementById("resultsCategory").value = 'You have chosen:' + `${category}`;
-
 }
 
 getCategory();
 
-const difficultyButton = document.getElementById('difficulty-btn');
-difficultyButton.addEventListener('click', getDifficultyLevel);
-
-//function to get difficulty level
+//function to get difficulty level for the fetch API request
 function getDifficultyLevel() {
     let difficultylevel = document.forms[1];
     difficulty = '';
@@ -122,27 +92,23 @@ function getDifficultyLevel() {
     // Save data to sessionStorage
     sessionStorage.setItem('difficulty', difficulty);
     //document.getElementById('resultsDifficulty').value = 'You have chosen:' + difficulty;
-    document.getElementById("resultsDifficulty").value = 'You have chosen, ' + `${difficulty}`;   
+    document.getElementById("resultsDifficulty").value = 'You have chosen, ' + `${difficulty}`;
 }
 
-getDifficultyLevel(); 
-
-
-// Fetch 10 questions from API from general knowledge category
+getDifficultyLevel();
+// Fetch 10 questions from API from general knowledge category which includes all catergories, 
+//and difficulty levels, catch all (default setting).
 async function getQuestions() {
 
     //pass variable to question variable line2
     const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`);
-    //const response = await fetch(`https://opentdb.com/api.php?amount=10&category=11&difficulty=hard&type=multiple`);
     questions = await response.json();
 }
-
 //Function to get next question, but also test to see if there is a next question 
-//to get, if not the game is over
+//to get, if not the game is over, and the next button is hidden
 function getNextQuestion() {
     const nextButton = document.getElementById('next-btn');
     nextButton.addEventListener('click', getNextQuestion);
-
     if (questionNumber > [questions.results.length]) {
         if (confirm('Game Over!')) {
             showFinalScore();
@@ -151,7 +117,7 @@ function getNextQuestion() {
             return;
         }
     }
-
+    //lets the user knows what question they are on and how many questions are being asked (max:10)
     document.getElementById('progress').innerText = `Question ${questionNumber}/${maxQuestions}`;
     ++questionNumber;
     //getCurrent question
@@ -180,17 +146,20 @@ function getNextQuestion() {
     }
 }
 
-//function to show the game interface
+//function to show the full game interface and hides the previous options and introductory text
 function startGame() {
     //Use Start button to show the gaming area
     let startButton = document.getElementById('start-btn');
     startButton.classList.add('hide');
     document.getElementById('scoreText').innerText = '';
     startButton.addEventListener('click', startGame => {
+       //shows the game itself
         const targetDiv = document.getElementById('hidden');
         targetDiv.style.display = 'block';
+        //hides earlier section but keeps the welcome text
         const targetDiv2 = document.getElementById('hiddenStart');
         targetDiv2.style.display = 'none';
+        //hides the final result screen till the game is over
         const targetDiv3 = document.getElementById('hiddenResults');
         targetDiv3.style.display = 'none';
     });
@@ -210,14 +179,12 @@ function choiceListeners() {
     });
 }
 
-async function init() {    
+async function init() {
     await getQuestions();
     startGame();
     getNextQuestion(0);
     choiceListeners();
     showFinalScore();
-   
-    
 }
 init();
 checkAnswers();
@@ -229,12 +196,10 @@ function checkAnswers() {
 
         if (button1.innerHTML === correctAnswer) {
             button1.style.background = 'green';
-            //alert('you are correct');
             showCorrectAnswer();
             incrementScore();
         } else {
             button1.style.background = 'red';
-           // alert('you are incorrect');
             showCorrectAnswer();
             incrementWrongAnswer();
         }
@@ -245,12 +210,10 @@ function checkAnswers() {
 
         if (button2.innerHTML === correctAnswer) {
             button2.style.background = 'green';
-            //alert('you are correct');
             showCorrectAnswer();
             incrementScore();
         } else {
             button2.style.background = 'red';
-            //alert('you are incorrect');
             showCorrectAnswer();
             incrementWrongAnswer();
         }
@@ -261,12 +224,10 @@ function checkAnswers() {
 
         if (button3.innerHTML === correctAnswer) {
             button3.style.background = 'green';
-            //alert('you are correct');
             showCorrectAnswer();
             incrementScore();
         } else {
             button3.style.background = 'red';
-            //alert('you are incorrect');
             showCorrectAnswer();
             incrementWrongAnswer();
         }
@@ -277,12 +238,10 @@ function checkAnswers() {
 
         if (button4.innerHTML === correctAnswer) {
             button4.style.background = 'green';
-           // alert('you are correct');
             showCorrectAnswer();
             incrementScore();
         } else {
             button4.style.background = 'red';
-           // alert('you are incorrect');
             showCorrectAnswer();
             incrementWrongAnswer();
         }
@@ -326,40 +285,35 @@ function incrementWrongAnswer() {
     index++;
 }
 
-
-//hide next button when there are no questions left in the game
+//hides next button when there are no questions left in the game
 function hideButton() {
-    document.getElementById('nextButton');    
+    document.getElementById('nextButton');
     if (questionNumber >= 10) {
-        nextButton.style.display = 'none';       
+        nextButton.style.display = 'none';
     } else {
-        nextButton.style.display = 'block';        
+        nextButton.style.display = 'block';
     }
 }
-
-const targetDiv3 = document.getElementById('hiddenResults');
-const targetDiv4 = document.getElementById('verify-answer');
-
 
 //When the quiz is over the score is shown to the user  
 //final scores to be displayed to user on ending the game 
 function showFinalScore() {
     if (questionNumber > [questions.results.length]) {
         //final scores to be displayed to user
-        targetDiv3.style.display = 'block';        
+        targetDiv3.style.display = 'block';
         targetDiv4.style.display = 'none';
         document.getElementById('scoreText').innerText = 'You have scored, ' + `${finalScore}` + '!';
         let closingMessage = document.getElementById('closing-message');
         if (finalScore == 0) {
-            closingMessage.innerHTML = "ahem....well, a little practise and you'll soon get there";
+            closingMessage.innerHTML = "Maybe quizzing is not your forte, have you tried Sudoku";
         } else if (finalScore < 3) {
-            closingMessage.innerHTML = 'Maybe quizzing is not your forte, have you tried Sudoku';
+            closingMessage.innerHTML = "ahem....well, a little practise and you'll soon get there";
         } else if (finalScore < 6) {
-            closingMessage.innerHTML = "Better luck next time, I'm sure you will get there";
+            closingMessage.innerHTML = "Better luck next time, I'm rooting for you";
         } else if (finalScore < 9) {
             closingMessage.innerHTML = 'So close, but no cigar, next time you will be the winner';
         } else if (finalScore == 10) {
-            closingMessage.innerHTML = 'You have achieved greatness';
+            closingMessage.innerHTML = 'You have achieved greatness, well done';
         }
     }
 }
@@ -370,11 +324,37 @@ sessionStorage.setItem('score', 'finalScore');
 
 showFinalScore();
 
-const restartButton = document.getElementById('restart-btn');
-restartButton.addEventListener('click', replay);
-
 //Reloads the page, so the game can start again
 function replay() {
     location.reload();
     console.log("Reload called")
 }
+
+//Modal for categories
+openModal.addEventListener('click', () => {
+    modal.showModal();
+});
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+});
+
+//Modal2 for difficulty
+openModal2.addEventListener('click', () => {
+    modal2.showModal();
+});
+
+closeModal2.addEventListener('click', () => {
+    modal2.close();
+});
+
+/*function getUserName() {
+    let username = prompt("Enter a username");
+    if (username != '') {
+        // Save data to sessionStorage
+        sessionStorage.setItem('username', username);
+        document.getElementById('welcomeText').innerHTML = 'Welcome ' + username + ' to Quiz-Lite';
+    } else {
+        alert('Username cannot be blank!');
+    }
+}*/
